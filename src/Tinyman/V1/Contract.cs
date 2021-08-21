25d08aa31eab7bbd577e098ebfe2007417f4dc6d
Geoff O'Donnell <gbosystems@outlook.com>
2021-08-21 11:56:28 -0400

    Adds augmented Algorand SDK classes to support TEAL 4 opcodes. Untested.

--- src/Tinyman/V1/Contract.cs
+++ src/Tinyman/V1/Contract.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using Tinyman.V1.Asc;
+using Tinyman.V1.Teal4;
 
 namespace Tinyman.V1 {
 
@@ -12,6 +13,8 @@ namespace Tinyman.V1 {
 		private const string ResourceFileName = "Tinyman.V1.asc.json";
 		private const string PoolLogicSigName = "pool_logicsig";
 		private const string ValidatorAppName = "validator_app";
+		private const string UnsupportedVersionErrorMessageFromSdk = "unsupported version";
+		private const string UnsupportedVersionErrorMessage = "Unsupported version -- use Contract.GetPoolLogicSig4 until the Algorand SDK is updated to support TEAL 4 opcodes.";
 
 		private static ContractCollection mContracts;
 		private static LogicSigContract PoolLogicSigDef;
@@ -33,8 +36,32 @@ namespace Tinyman.V1 {
 				{ "asset_id_2", assetIdMin }
 			});
 
-			// TODO: This is currently not supported by the Algorand SDK -- Something to do with TEAL 4?
-			return new LogicsigSignature(logic: bytes);
+			try {
+				return new LogicsigSignature(logic: bytes);
+
+			} catch (ArgumentException ex) {
+
+				if (String.Equals(ex.Message, UnsupportedVersionErrorMessageFromSdk)) {
+					throw new ArgumentException(UnsupportedVersionErrorMessage, ex);
+				}
+
+				throw;
+			}
+		}
+
+		public static LogicsigSignature4 GetPoolLogicSig4(
+			long validatorAppId, long assetIdA, long assetIdB) {
+
+			var assetIdMax = Math.Max(assetIdA, assetIdB);
+			var assetIdMin = Math.Min(assetIdA, assetIdB);
+
+			var bytes = Util.GetProgram(PoolLogicSigDef.Logic, new Dictionary<string, object> {
+				{ "validator_app_id", validatorAppId },
+				{ "asset_id_1", assetIdMax },
+				{ "asset_id_2", assetIdMin }
+			});
+
+			return new LogicsigSignature4(logic: bytes);
 		}
 
 		private static void LoadContractsFromResource() {
---
+++ src/Tinyman/V1/Teal4/Digester.cs
@@ -0,0 +1,24 @@
+﻿using Org.BouncyCastle.Crypto.Digests;
+
+namespace Tinyman.V1.Teal4 {
+
+    /// <summary>
+    /// Copied from: https://github.com/RileyGe/dotnet-algorand-sdk/blob/master/dotnet-algorand-sdk/Util/Digester.cs
+    /// to support TEAL4 contracts in the Tinyman SDK until the Algorand SDK is updated, at which time this class
+    /// will be removed.
+    /// </summary>
+    internal class Digester {
+
+        internal static byte[] Digest(byte[] data) {
+
+            Sha512tDigest digest = new Sha512tDigest(256);
+            digest.BlockUpdate(data, 0, data.Length);
+            byte[] output = new byte[32];
+            digest.DoFinal(output, 0);
+
+            return output;
+        }
+
+    }
+
+}
