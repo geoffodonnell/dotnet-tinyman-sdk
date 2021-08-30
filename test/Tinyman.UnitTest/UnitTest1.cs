d7d6e9f1979a1bfc3df5c2ea192e43dfba46df17
Geoff O'Donnell <odonne89@outlook.com>
2021-08-30 19:42:59 -0400

    Adds solution for tests

---
+++ test/Tinyman.UnitTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tinyman.UnitTest {
+	[TestClass]
+	public class UnitTest1 {
+		[TestMethod]
+		public void TestMethod1() {
+		}
+	}
+}
