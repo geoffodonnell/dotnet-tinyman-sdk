780b3a9afcfb5351ed2d8692fe7e0e35deabcd82
Geoff O'Donnell <odonne89@outlook.com>
2021-10-08 17:31:48 -0400

    Adds mainnet validator app ID

--- src/Tinyman/V1/Constant.cs
+++ src/Tinyman/V1/Constant.cs
@@ -6,7 +6,7 @@
 		public const string AlgodMainnetHost = "https://api.algoexplorer.io";
 		
 		public const ulong TestnetValidatorAppId = 21580889;
-		public const ulong MainnetValidatorAppId = 0;
+		public const ulong MainnetValidatorAppId = 350338509;
 
 		public const ulong BurnFee = 3000;
 		public const ulong MintFee = 2000;
